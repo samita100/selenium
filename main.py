e05dc847f90b326ab957e82a844a3748b1f139c9
samita100 <66769151+samita100@users.noreply.github.com>
2020-11-01 09:15:11 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import requests
+import time
+
+response = requests.get("https://xrohan.me/proxy")
+
+proxy = response.text
+
+
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--allow-popups-during-page-unload")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--enable-auto-reload")
+chrome_options.add_argument(f'--proxy-server={proxy}')
+
+
+driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
+
+driver.get("https://www.alexamaster.net/ads/autosurf/117735")
+
+time.sleep(590)
+
+
+driver.quit()
