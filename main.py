b44b0715f452c30500c538af3e2166ad432695be
samita100 <66769151+samita100@users.noreply.github.com>
2020-11-03 14:58:48 +0545

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def first():
 
 
   chrome_options = webdriver.ChromeOptions()
-  #chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--headless")
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
   chrome_options.add_argument("--allow-popups-during-page-unload")
